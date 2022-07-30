7b5a6624d9ba76a08ee3d42ca6c75d8f9ad4d233
Haoli Yin <haoli.yin@vanderbilt.edu>
2022-07-30 17:51:22 -0500

    new run config

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -12,20 +12,21 @@ import time
 import torch
 import torch.optim as optim
 
+num_epochs = 1
+initial_lr = 0.001
+batch_size = 32
+
 if __name__ == '__main__':
 
     img_dir = Path(r"D:\GLENDA_v1.5_no_pathology\no_pathology\GLENDA_img")
     mask_dir = Path(r"D:\GLENDA_v1.5_no_pathology\no_pathology\GLENDA_mask")
 
-    train_loader, val_loader, test_loader = create_dataloaders(img_dir, mask_dir, batch_size=4)
+    train_loader, val_loader, test_loader = create_dataloaders(img_dir, mask_dir, batch_size)
     model = NoPoolASPP()
-    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"
-    device = "cpu"
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
     model.to(device)
     print(device)
 
-    num_epochs = 1
-    initial_lr = 0.001
     optimizer = optim.Adam(model.parameters(), lr=initial_lr)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, num_epochs)
 
@@ -50,8 +51,8 @@ if __name__ == '__main__':
             train_loss_total += loss.item()
             num_steps += 1
 
-            if i % 2 == 0:
-                print("Epoch: {}, Step: {}, Loss: {}".format(epoch, i, loss.item()))
+            if i % 5 == 0:
+                print("Epoch: {}, Step: {}, Loss: {}".format(epoch, i, -loss.item()))
 
         train_loss_total_avg = train_loss_total / num_steps
         # run validation loop for one epoch
