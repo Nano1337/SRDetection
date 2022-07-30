35a0f42b958b4dbb50be8475e460a89b10b6f23e
Haoli Yin <haoli.yin@vanderbilt.edu>
2022-07-30 16:45:16 -0500

    Write train loop

---
+++ src/models/losses.py
@@ -0,0 +1,18 @@
+def dice_loss(input, target):
+    """Dice loss.
+
+    :param input: The input (predicted)
+    :param target:  The target (ground truth)
+    :returns: the Dice score between 0 and 1.
+    """
+    eps = 0.0001
+
+    iflat = input.view(-1)
+    tflat = target.view(-1)
+
+    intersection = (iflat * tflat).sum()
+    union = iflat.sum() + tflat.sum()
+
+    dice = (2.0 * intersection + eps) / (union + eps)
+
+    return -dice
---
+++ src/models/train_model.py
@@ -0,0 +1,70 @@
+# import modules from outside
+from loading_data import create_dataloaders
+from architecture import NoPoolASPP
+from losses import dice_loss
+
+# system libraries 
+from pathlib import Path
+from tqdm import tqdm
+import time
+
+# deep learning libraries
+import torch
+import torch.optim as optim
+
+if __name__ == '__main__':
+
+    img_dir = Path(r"D:\GLENDA_v1.5_no_pathology\no_pathology\GLENDA_img")
+    mask_dir = Path(r"D:\GLENDA_v1.5_no_pathology\no_pathology\GLENDA_mask")
+
+    train_loader, val_loader, test_loader = create_dataloaders(img_dir, mask_dir, batch_size=4)
+    model = NoPoolASPP()
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"
+    device = "cpu"
+    model.to(device)
+    print(device)
+
+    num_epochs = 1
+    initial_lr = 0.001
+    optimizer = optim.Adam(model.parameters(), lr=initial_lr)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, num_epochs)
+
+    for epoch in tqdm(range(1, num_epochs+1)):
+        start_time = time.time()
+        scheduler.step()
+
+        lr = scheduler.get_lr()[0]
+
+        model.train()
+        train_loss_total = 0.0
+        num_steps = 0
+        
+        # run train loop for one epoch
+        for i, batch in enumerate(train_loader):
+            img, mask = batch[0].to(device), batch[1].to(device)
+            optimizer.zero_grad()
+            pred = model(img)
+            loss = dice_loss(pred, mask)
+            loss.backward()
+            optimizer.step()
+            train_loss_total += loss.item()
+            num_steps += 1
+
+            if i % 2 == 0:
+                print("Epoch: {}, Step: {}, Loss: {}".format(epoch, i, loss.item()))
+
+        train_loss_total_avg = train_loss_total / num_steps
+        # run validation loop for one epoch
+        model.eval()
+        val_loss_total = 0.0
+        num_steps = 0
+
+        for i, batch in enumerate(val_loader):
+            img, mask = batch[0].to(device), batch[1].to(device)
+            pred = model(img)
+            loss = dice_loss(pred, mask)
+            val_loss_total += loss.item()
+            num_steps += 1
+
+        val_loss_total_avg = val_loss_total / num_steps
+        print("Epoch: {}, Train Loss: {}, Val Loss: {}".format(epoch, train_loss_total_avg, val_loss_total_avg))
